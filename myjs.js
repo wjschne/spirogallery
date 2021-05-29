0d5d222ada26293d212ae2697e48b6efadc7a88d
W. Joel Schneider <w.joel.schneider@gmail.com>
2021-05-29 13:15:45 -0400

    new buttons

--- myjs.js
+++ myjs.js
@@ -1,11 +1,24 @@
-var obs = document.getElementsByTagName("object");
 
-var i;
-for (i = 0; i < obs.length; i++) {
-  var o = obs[i]
-  var s = o.contentDocument.getElementsByTagName("svg")
-  if (s.length > 0) {s[0].pauseAnimations()}
-}
-
-document.getElementById("osloaster").contentDocument.getElementsByTagName("svg")[0].pauseAnimations()
-
+slideshow.on('hideSlide', function (slide) {
+  // Slide is the slide being navigated away from
+  
+  var obs = document.getElementsByClassName("spiro");
+  var i;
+  for (i = 0; i < obs.length; i++) {
+    var o = obs[i]
+    var d = o.contentDocument
+    if (d != null) {
+      var s = d.documentElement
+      s.pauseAnimations();
+    }
+    
+  }
+  
+  var btns = document.getElementsByClassName("playbtn")
+  for (i = 0; i < btns.length; i++) {
+    var btn = btns[i]
+    btn.innerHTML = "Play";
+  }
+  
+  
+});
